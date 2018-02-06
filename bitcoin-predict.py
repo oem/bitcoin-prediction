e0413f848aa4254d04b31952261e7badacbe1487
Ömür Özkir <oemuer.oezkir@gmail.com>
2018-02-06 16:02:08 +0100

    moves some of the set generation code into their own files

--- bitcoin-predict.py
+++ bitcoin-predict.py
@@ -1,5 +1,5 @@
 from src.data.load import load_csv
-from src.data.sets import calc_test_size, calc_training_size
+from src.data.sets import training_set, calc_test_size, test_set
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import pandas as pd
@@ -18,31 +18,13 @@ TIME_STEPS = 30
 TEST_SIZE = 0.2
 
 df = load_csv("./data/all_time.csv")
-training_set_length = calc_training_size(len(df), BATCH_SIZE, TEST_SIZE)
-train_with_padding = training_set_length + TIME_STEPS * 2
-df_train = df[0:train_with_padding]
-training_set = df_train.iloc[:, 1:].values
-sc = MinMaxScaler(feature_range=(0, 1))
-scaled_training_set = sc.fit_transform(training_set)
-
-X_train = []
-y_train = []
-for i in range(TIME_STEPS, training_set_length + TIME_STEPS):
-    X_train.append(scaled_training_set[i - TIME_STEPS:i, 0])
-    y_train.append(scaled_training_set[i:i + TIME_STEPS, 0])
-
-X_train, y_train = np.array(X_train), np.array(y_train)
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
+X_train, y_train = training_set(df, BATCH_SIZE, TEST_SIZE, TIME_STEPS)
 
 inputs_1_mae = Input(batch_shape=(BATCH_SIZE, TIME_STEPS, 1))
 lstm_1_mae = LSTM(10, stateful=True, return_sequences=True)(inputs_1_mae)
 lstm_2_mae = LSTM(10, stateful=True, return_sequences=True)(lstm_1_mae)
-
 output_1_mae = Dense(units=1)(lstm_2_mae)
-
 regressor_mae = Model(inputs=inputs_1_mae, outputs=output_1_mae)
-
 regressor_mae.compile(optimizer='adam', loss='mae')
 for i in range(EPOCHS):
     print("Epoch: " + str(i))
@@ -50,26 +32,17 @@ for i in range(EPOCHS):
                       epochs=1, batch_size=BATCH_SIZE)
     regressor_mae.reset_states()
 
-testset_length = calc_test_size(
-    len(df), BATCH_SIZE, TIME_STEPS, train_with_padding)
-test_with_padding = testset_length + TIME_STEPS * 2 + train_with_padding
-
-df_test = df[train_with_padding:test_with_padding]
-test_set = df_test.iloc[:, 1:].values
-
-scaled_test_values = sc.fit_transform(test_set)
-
-X_test = []
-for i in range(TIME_STEPS, testset_length + TIME_STEPS):
-    X_test.append(scaled_test_values[i - TIME_STEPS:i, 0])
-X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+X_test, test_set, sc = test_set(df, BATCH_SIZE, TIME_STEPS, X_train)
 predicted_mae = regressor_mae.predict(X_test, batch_size=BATCH_SIZE)
 regressor_mae.reset_states()
 
 predicted_mae = np.reshape(
     predicted_mae, (predicted_mae.shape[0], predicted_mae.shape[1]))
 predicted_mae = sc.inverse_transform(predicted_mae)
+
+padded_train_size = len(X_train) + TIME_STEPS * 2
+testset_length = calc_test_size(
+    len(df), BATCH_SIZE, TIME_STEPS, padded_train_size)
 
 y_test = []
 for j in range(0, testset_length - TIME_STEPS):
