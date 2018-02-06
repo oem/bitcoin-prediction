1ab9eba11dfa448fb16215bd7a40e2f5dc83a939
Ömür Özkir <oemuer.oezkir@gmail.com>
2018-02-06 17:17:09 +0100

    cleanup of the script to generate the neural network and some other minor cleanups

--- bitcoin-predict.py
+++ bitcoin-predict.py
@@ -1,5 +1,6 @@
 from src.data.load import load_csv
 from src.data.sets import training_set, calc_test_size, test_set
+from src.models.train import train_regressor_mae
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import pandas as pd
@@ -20,21 +21,13 @@ TEST_SIZE = 0.2
 df = load_csv("./data/all_time.csv")
 X_train, y_train = training_set(df, BATCH_SIZE, TEST_SIZE, TIME_STEPS)
 
-inputs_1_mae = Input(batch_shape=(BATCH_SIZE, TIME_STEPS, 1))
-lstm_1_mae = LSTM(10, stateful=True, return_sequences=True)(inputs_1_mae)
-lstm_2_mae = LSTM(10, stateful=True, return_sequences=True)(lstm_1_mae)
-output_1_mae = Dense(units=1)(lstm_2_mae)
-regressor_mae = Model(inputs=inputs_1_mae, outputs=output_1_mae)
-regressor_mae.compile(optimizer='adam', loss='mae')
-for i in range(EPOCHS):
-    print("Epoch: " + str(i))
-    regressor_mae.fit(X_train, y_train, shuffle=False,
-                      epochs=1, batch_size=BATCH_SIZE)
-    regressor_mae.reset_states()
+regressor = train_regressor_mae(
+    X_train, y_train, BATCH_SIZE, TIME_STEPS, EPOCHS)
 
 X_test, test_set, sc = test_set(df, BATCH_SIZE, TIME_STEPS, X_train)
-predicted_mae = regressor_mae.predict(X_test, batch_size=BATCH_SIZE)
-regressor_mae.reset_states()
+
+predicted_mae = regressor.predict(X_test, batch_size=BATCH_SIZE)
+regressor.reset_states()
 
 predicted_mae = np.reshape(
     predicted_mae, (predicted_mae.shape[0], predicted_mae.shape[1]))
@@ -54,4 +47,4 @@ rmse = math.sqrt(mean_squared_error(
     test_set[TIME_STEPS:len(y_test)], y_test[0:len(y_test) - TIME_STEPS]))
 print("RMSE:", rmse)
 
-regressor_mae.save(filepath="models/01_with_mae.h5")
+regressor.save(filepath="models/01_with_mae.h5")
