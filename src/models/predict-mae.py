a5dfa33734576a837a08124a2bc8ccfdc5017b0d
Ömür Özkir <oemuer.oezkir@gmail.com>
2018-02-07 12:03:51 +0100

    bit of cleanup of the prediction script

--- src/models/predict-mae.py
+++ src/models/predict-mae.py
@@ -1,16 +1,8 @@
 from ..data.load import load_csv
 from ..data.sets import prepare_data_lstm
-from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import h5py
-import math
-from sklearn.metrics import mean_squared_error
 from keras.models import load_model
-
-BATCH_SIZE = 1
-EPOCHS = 15
-TIME_STEPS = 1
-TEST_SIZE = 0.2
 
 # prepare the training and test set
 df = load_csv("data/all_time_daily.csv")
@@ -22,18 +14,7 @@ predicted_train = regressor.predict(X_train)
 predicted_test = regressor.predict(X_test)
 
 one_day_predict = regressor.predict(np.asarray([[y_test[-1]]]))
-print(sc.inverse_transform(y_test)
-      [-1], sc.inverse_transform(one_day_predict).flatten())
-
-predicted_train = sc.inverse_transform(predicted_train)
-predicted_test = sc.inverse_transform(predicted_test)
-y_train = sc.inverse_transform(y_train)
-y_test = sc.inverse_transform(y_test)
-
-# measure
-train_score = math.sqrt(mean_squared_error(
-    y_train[:, 0], predicted_train[:, 0]))
-test_score = math.sqrt(mean_squared_error(
-    y_test[:, 0], predicted_test[:, 0]))
-print("Train Score: %.2f RMSE" % train_score)
-print("Test Score: %.2f RMSE" % test_score)
+print("Last closing price: %.2f US Dollar" %
+      sc.inverse_transform(y_test)[-1][0])
+print('Predicted next closing price: %.2f US Dollar ' %
+      sc.inverse_transform(one_day_predict).flatten()[0])
